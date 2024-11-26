e9238e91225e84d8965343f5d7cab6c642e925e9
W8jonas <jonashenrique989@gmail.com>
2024-11-25 21:53:32 -0300

    Add 04 e 05

--- submission/004.sh
+++ submission/004.sh
@@ -2,11 +2,11 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-path="/0/*"
-descriptor="tr($xpub$path)"
+descriptor_base="tr($xpub/0/*)"
 
-checksum=$(bitcoin-cli getdescriptorinfo "$descriptor" | jq -r '.checksum')
-full_descriptor="tr($xpub$path)#$checksum"
-address=$(bitcoin-cli deriveaddresses "$full_descriptor" "[100,100]" | jq -r '.[0]')
+checksum=$(bitcoin-cli getdescriptorinfo "$descriptor_base" | jq -r '.checksum')
+descriptor_with_checksum="${descriptor_base}#${checksum}"
+
+address=$(bitcoin-cli deriveaddresses "$descriptor_with_checksum" "[100,100]")
 
 echo $address
--- submission/005.sh
+++ submission/005.sh
@@ -1,2 +1,15 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
+
+TXID="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
+
+RAW_TX=$(bitcoin-cli getrawtransaction "$TXID" true)
+PUBKEYS=$(echo "$RAW_TX" | jq -r '.vin[].txinwitness[1]' | sort -u)
+
+PUBKEY_ARRAY=$(echo "$PUBKEYS" | jq -R . | jq -s .)
+MULTISIG=$(bitcoin-cli createmultisig 1 "$PUBKEY_ARRAY" "p2sh-segwit")
+
+P2SH_ADDRESS=$(echo "$MULTISIG" | jq -r '.address')
+REDEEM_SCRIPT=$(echo "$MULTISIG" | jq -r '.redeemScript')
+
+echo "$P2SH_ADDRESS"
